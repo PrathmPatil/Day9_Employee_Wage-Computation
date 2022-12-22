590cf58dafc2bbdef75b1e33dd00d3ac536f3874
PrathmPatil <prathmpatil2818@gmail.com>
2022-12-23 00:12:03 +0530

    Check Employee are Present Or Not

--- src/com/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/employeewagecomputation/EmployeeWageComputation.java
@@ -1,10 +1,27 @@
 package com.employeewagecomputation;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
 
+	public static  int randomnumber(int a)
+	{
+		Random random =new Random();
+		a=random.nextInt(2);
+		return a;
+		
+	}
+	public static void presenti(int a) {
+		int num=randomnumber(a);
+		if(num==1)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("**********Wel-Come In Employee Wage Computation**********");
+         presenti(randomnumber(0));
 	}
 
 }
