f569d9ca86cd0c6762d4737648607477eb701e34
PrathmPatil <prathmpatil2818@gmail.com>
2022-12-23 01:24:03 +0530

    Calsulate 20 working  day's Wage of Employee

--- src/com/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/employeewagecomputation/EmployeeWageComputation.java
@@ -3,37 +3,18 @@ package com.employeewagecomputation;
 import java.util.Random;
 
 public class EmployeeWageComputation {
-
-	public static  int randomnumber(int a)
-	{ 
-		Random random =new Random();
-		a=random.nextInt(2);
-		return (a);
-		
-	}
-	public static  int randPartTime(int a)
-	{ 
-		Random random =new Random();
-		a=random.nextInt(2);
-		return (a);
-		
-	}
-	public static void presenti(int a) {
-		int num=randomnumber(a);
-		switch(num) {
-		case 1: System.out.println("Employee Present");
-		       System.out.println("Wage of Employee is "+(20*8));
-		       break;
-		case 0:System.out.println("Employe are Absent");
-		       System.out.println("Wage of Employee is "+0);
-		}
-		
-		
-	}
+ 
 	public static void main(String[] args) {
 		
 		System.out.println("**********Wel-Come In Employee Wage Computation**********");
-         presenti(randomnumber(0));
+         int totalday=0;
+         for(int i=0;i<20;i++) {
+     		Random random =new Random();
+     		int a=random.nextInt(2);
+             if(a==1) {
+            	 totalday++;
+             }
 	}
-
+         System.out.println("Total Wage of the month is "+totalday*8*20);
+	}
 }
