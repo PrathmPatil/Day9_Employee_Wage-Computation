7aa5827cfde110882c3762ea578c795696b772cc
PrathmPatil <prathmpatil2818@gmail.com>
2022-12-23 13:24:29 +0530

    Multiple Companies wage and days

--- src/com/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/employeewagecomputation/EmployeeWageComputation.java
@@ -1,30 +1,44 @@
 package com.employeewagecomputation;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class EmployeeWageComputation {
-  public static void month(int hr)
-  {
-	  if(hr>100 ) {
-		  System.out.println("Total Wage in this month of the Employee is "+(hr*20));
-	  }
-	  else {
-		  System.out.println("Employee have not fullfill Day and Hours requirment ");
-	  }
+	static int totalday=0;
+	/*
+	 * For multiple Companies
+	 */
+	 public static int Company(String name, int days, int wage) {
+		 int a;
+		 System.out.println("Name Of Company is :: "+name);
+		 System.out.println("Working Days in Month is :: "+days);
+		 System.out.println("Wage per hour is :: "+wage);
+		 for(int i=0;i<days;i++) 
+		 {
+	     		Random random =new Random();
+	     		 a=random.nextInt(2);
+	     		if(a==1) 
+	     		 {
+	            	 totalday++;
+	             }
+	     }
+		 return totalday;
+	 }
+  public static void month(int totalday,int wage)
+  {     
+	 System.out.println("Total Wage of the month is :: "+totalday*wage*8);
+	  
   }
 	public static void main(String[] args) {
-		
+		Scanner sc=new Scanner(System.in);
 		System.out.println("**********Wel-Come In Employee Wage Computation**********");
-         int totalday=0;
-         for(int i=0;i<20;i++) {
-     		Random random =new Random();
-     		int a=random.nextInt(2);
-     		System.out.println(a);
-             if(a==1) {
-            	 totalday++;
-            	 System.out.println(totalday);
-             }
-	}
-         month(totalday*8);
+		System.out.println("Enter Company Name ::-");
+		String cName=sc.next();
+		System.out.println("Enter Monthly Days of Working ::-");
+		int mDays=sc.nextInt();
+		System.out.println("Enter Wage Per Hours ::-");
+		int wHour=sc.nextInt();
+		int totaldays= Company(cName,mDays,wHour);
+	      month(totaldays,wHour);
 	}
 }
